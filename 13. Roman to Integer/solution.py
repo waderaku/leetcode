ec5bead95771a935d10fcd6cd37405b976f25940
waderaku <wadebosh@icloud.com>
2022-10-27 14:57:48 +0000

    コメントに問題のリンクを追加

--- 13. Roman to Integer/solution.py
+++ 13. Roman to Integer/solution.py
@@ -1,21 +1,4 @@
-# def create_special_roman_int_map(roman_int_map: dict[str, int]) -> dict[str, int]:
-#     key_list = list(roman_int_map.keys())
-#     special_roman_int_map = {}
-#     for index, roman in enumerate(key_list):
-#         if len(key_list) - 3 < index:
-#             break
-
-#         next_roman = key_list[index + 1]
-#         two_next_roman = key_list[index + 2]
-
-#         value = roman_int_map[roman]
-#         next_value = roman_int_map[next_roman]
-#         two_next_value = roman_int_map[two_next_roman]
-
-#         special_roman_int_map[f"{roman}{next_roman}"] = next_value - value
-#         special_roman_int_map[f"{roman}{two_next_roman}"] = two_next_value - value
-
-#     return special_roman_int_map
+# dhttps://leetcode.com/problems/roman-to-integer/
 
 
 class Solution:
@@ -57,6 +40,3 @@ class Solution:
 
         target_s = s[:2]
         return self.special_roman_int_map.get(target_s, 0)
-
-
-print(Solution().romanToInt("MCDLXXVI"))
